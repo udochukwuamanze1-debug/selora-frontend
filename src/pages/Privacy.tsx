73ebd555ed1db86d1f6cd4a7f1e14f4140f49aaa
udochukwuamanze1-debug <udochukwuamanze1@gmail.com>
2025-12-27 14:49:29 +0100

    Update Privacy.tsx

--- src/pages/Privacy.tsx
+++ src/pages/Privacy.tsx
@@ -60,7 +60,7 @@ const Privacy = () => {
               </p>
 
               <p className="text-sm text-muted-foreground mt-8">
-                Last updated: December 2024
+                Last updated: December 2025
               </p>
             </div>
           </div>
